ef838627f9a713dfc6be46ff4ec2e4ca9e693e77
Carl Gödecken <carl.julius.g@me.com>
2018-06-06 09:54:44 +0200

    Update benchmark script for Intel SMT (Hyperthreading)

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,42 +3,51 @@
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 if lscpu | grep -E '^Model name\:\s+POWER'; then
-#if true; then
   IS_POWER=true
   FOLDER="power-results"
-else
-  IS_POWER=false
-  FOLDER="intel-results"
-fi
-
-# schedule 8 threads at most to avoid NUMA side-effects
-PREFETCHER_SETTINGS=(1 0) # be careful: 0 is standard, and 1 is off!
-SMT_SETTINGS=(1 2 4 8)
-THREAD_COUNTS=(8 16 32 64)
-# use seq start+offset 8 end for generating these sequences for taskset
-CORE_BINDINGS=("`seq -s, 0 8 63`"
+  SMT_SETTINGS=(1 2 4 8)
+  # use seq start+offset 8 end for generating these sequences for taskset
+  CORE_BINDINGS=("`seq -s, 0 8 63`"
                  "`seq -s, 0 8 63`,`seq -s, 1 8 63`"
                  "`seq -s, 0 8 63`,`seq -s, 1 8 63`,`seq -s, 2 8 63`,`seq -s, 3 8 63`"
                  "`seq -s, 0 63`")
+  SMT_CONFIGURATIONS=4
+  THREAD_COUNTS=(8 16 32 64)
+  # schedule 8 threads at most to avoid NUMA side-effects
+  PREFETCHER_SETTINGS=(1 0) # be careful: 0 is standard, and 1 is off!
+else
+  IS_POWER=false
+  FOLDER="intel-results"
+  PREFETCHER_SETTINGS=(1 0) # be careful: 0 is standard, and 1 is off!
+  # on the rapa.eaalab machine, these are the NUMA node0 CPUs
+  SMT_CONFIGURATIONS=2
+  CORE_BINDINGS=("`seq -s, 0 1 8`"
+                 "`seq -s, 0 1 8`,`seq -s, 120 8 134`")
+  THREAD_COUNTS=(8 16)
+  SMT_SETTINGS=(1 2)
+fi
+
+
+
 mkdir -p "$FOLDER"
 
 for PREFETCH_SET in "${PREFETCHER_SETTINGS[@]}"; do
   if $IS_POWER; then
 	sudo ppc64_cpu --dscr="$PREFETCH_SET"
   else
-    if $PREFETCH_SET; then
+    if [[ $PREFETCH_SET ]]; then
 		benchmark/prefetching_intel -d
 	else
 		benchmark/prefetching_intel -e
 	fi
   fi  
 
-  for INDEX in `seq 0 3`; do
-    NTHREADS="${THREAD_COUNTS[$INDEX]}"
-    CORE_BIND="${CORE_BINDINGS[$INDEX]}"
+  for ((i=0; i<$SMT_CONFIGURATIONS; i++)); do
+    NTHREADS="${THREAD_COUNTS[i]}"
+    CORE_BIND="${CORE_BINDINGS[i]}"
+    SMTLVL="${SMT_SETTINGS[i]}"
 
 	if $IS_POWER; then
-      SMTLVL="${SMT_SETTINGS[$INDEX]}"
       sudo ppc64_cpu --smt="$SMTLVL"
 	fi
 	
