db4740af0cc37387c247baf9204ac7dfc0c5238c
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2018-06-11 12:24:30 +0200

    Fix oder of plot labels and scale plots depending on benchmark

--- generate_figures.py
+++ generate_figures.py
@@ -6,6 +6,12 @@ import numpy as np
 import pandas as pd
 
 
+tkey = 'Time in ns'
+colszkey = 'Column size in KB'
+dtypekey = 'Data type'
+colors = ['red', 'green', 'blue', 'cyan']
+
+
 def process_file(filename):
     data = pd.read_csv(filename)
     csizes = np.unique(data[colszkey])
@@ -36,30 +42,36 @@ def process_file(filename):
     plt.xscale('log', basex=2)
     plt.gca().xaxis.grid(True)
     plt.ylabel('Effective Scan Bandwidth (in GB/s)')
+
     if system_type == 'intel':
-        plt.ylim(ymin=0, ymax=60)
+        if 'caching1' in filename:
+            plt.ylim(ymin=0, ymax=60)
+        else:
+            plt.ylim(ymin=0, ymax=20)
     else:
         plt.ylim(ymin=0, ymax=100)
-    plt.legend(loc=2, prop={'size': 10})
+
+    # print labels in the right order
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [3, 0, 1, 2]
+    plt.legend([handles[i] for i in order], [labels[i] for i in order], loc=2, prop={'size': 10})
+
     plt.savefig(filename.replace('.csv', '.png'))
     plt.clf()
 
 
 if __name__ == '__main__':
-    tkey = 'Time in ns'
-    colszkey = 'Column size in KB'
-    dtypekey = 'Data type'
-    colors = ['red', 'green', 'blue', 'cyan']
-
     path = sys.argv[1] if len(sys.argv) > 1 else 'benchmark.csv'
     system_type = sys.argv[2] if len(sys.argv) > 2 else 'power'
     try:
         if os.path.isdir(path):
             for filename in os.listdir(path):
                 if filename[-4:] == '.csv':
-                    process_file(path + filename)
+                    print('Plotting ' + filename + '...')
+                    process_file(os.path.join(path, filename))
         else:
             process_file(path)
+        print('Done')
     except FileNotFoundError as e:
         print(e)
         print('Usage: python generate_figures.py path [machine]')
